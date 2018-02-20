c49947480e718813d1dd974df32a674e2196f8f6
Yingbo <libbyandhelen@163.com>
2018-02-19 18:22:10 -0800

    change datablse config

--- timetable/timetable/settings.py
+++ timetable/timetable/settings.py
@@ -99,9 +99,9 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'timetable',  ## 数据库名称
-        'USER': 'root',
-        'PASSWORD': 'zhangyingbo',  ## 安装 mysql 数据库时，输入的 root 用户的密码
+        'NAME': 'timetable',  ## database name
+        'USER': 'timetable',
+        'PASSWORD': 'timetable',
         'HOST': 'localhost',
     }
 }
